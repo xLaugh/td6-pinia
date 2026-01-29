48651bf674b3a1cad0b5da4bf9dfa04a81188a14
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-01-29 16:32:31 +0100

    Ajout de vue3-toastify au main.js

--- timely/src/main.js
+++ timely/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import './style.css'
@@ -8,7 +10,8 @@ import './style.css'
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App)
-  .use(pinia)
-  .use(router)
-  .mount('#app')
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.use(Vue3Toastify, { autoClose: 3000 })
+app.mount('#app')
